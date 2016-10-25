9488664f112f438d10d73079778ca692710ca810
Sfen Helaers <sfenhelaers@gmail.com>
2016-10-25 11:06:09 +0200

    added styling for home and viewer page

--- app/components/index.js
+++ app/components/index.js
@@ -1,9 +1,11 @@
 import angular from 'angular';
-import home from './home';
 
+import home from './home';
+import viewer from './viewer';
 
 const module = angular.module('digibw.components', [
     home,
+    viewer,
 ]);
 
 export default module.name;
--- app/components/viewer/viewer.controller.js
+++ app/components/viewer/viewer.controller.js
@@ -7,13 +7,22 @@ function ViewerController($log, toaster) {
     // variables
     vm.title = 'This is viewer';
 
+    vm.currentSlide = 0; // deze id zou ik van de slide.component moeten halen -- hoe? /
+    vm.totalSlides = 4;
+
+    vm.toggleCarrousel = toggleCarrousel;
+
 
     activate();
 
     /////////
 
     function activate() {
-        toaster.pop('success', 'title', 'viewer shit');
+        toaster.pop('success', 'title', 'viewer text in box');
+    }
+
+    function toggleCarrousel() {
+        $log.log('this is the carrousel');
     }
 
 }
