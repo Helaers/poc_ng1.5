3be81643df8110a95db1ab2b7681fd7413a049c4
Sfen Helaers <sfenhelaers@gmail.com>
2016-10-27 10:31:19 +0200

    fixed editor styling + added thumbs to page (and controller)

--- app/components/editor/editor.controller.js
+++ app/components/editor/editor.controller.js
@@ -1,11 +1,18 @@
 //controller
-EditorController.$inject = ['$log', 'toaster'];
-function EditorController($log, toaster) {
+EditorController.$inject = ['$log', 'toaster', 'slidesService'];
+function EditorController($log, toaster, slidesService) {
 
     const vm = this;
 
     // variables
     vm.title = 'This is editor';
+
+    vm.totalSlides = 0;
+    vm.slides = [];
+
+    // function
+    vm.toggleVisibility = toggleVisibility;
+    vm.remove = remove;
 
 
     activate();
@@ -14,6 +21,25 @@ function EditorController($log, toaster) {
 
     function activate() {
         toaster.pop('success', 'title', 'editor here');
+
+        vm.slides = slidesService.getAll();
+        vm.totalSlides = vm.slides.length;
+    }
+
+    function toggleVisibility(id) {
+        slidesService.toggleVisibility(id);
+        vm.slides = slidesService.getAll();
+    }
+
+    function remove(slide) {
+        slidesService.removeSlide(slide);
+        orderSlides();
+    }
+
+    function orderSlides() {
+        for (let i = 0; i < vm.slides.length; i++) {
+            vm.slides[i].id = i + 1;
+        }
     }
 
 }
